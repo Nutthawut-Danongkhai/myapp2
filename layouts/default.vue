4d448b1a84804ebb2382bde83d1c36386988bcad
Nutthawut-Danongkhai <66309010009@tatc.ac.th>
2023-08-21 08:48:48 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">about us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/limitmessage">limitmessage</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </div>
+  
+  <slot />
+  <footer class="bg-dark text-center text-white fixed-bottom">
+    <a>จัดทำโดย</a><br>
+    <a>นาย ณัฐวุฒิ ดาหนองคาย</a><br>
+    <a>แผนกวิชา เทคโนโลยีสารสนเทศ</a>
+    <a>ระดับชั้น ปวส.1</a>
+  </footer>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+  <img :src="picture" :width="size" :height="size" />
+  <p>ชื่อ-นามสกุล : {{ getFullname() }}</p>
+  <p>นามสกุล : {{ age }}</p>
+  <button @clik="toggleVisible">{{ isVisible? "ซ่อน" : "แสดง" }}รายละเอียด</button>
+ <div v-show="toggleVisible">
+  <p>ที่อยู่ : <span v-html="address"></span></p>
+  <p v-if></p>
+  </div> 
+  </div>
